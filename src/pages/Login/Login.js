2674ffeaa53ae312e873cdbd57316c13930b8b0b
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-03-16 14:08:59 +0530

    add: navbar to login/signup page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,28 +1,32 @@
 import "./login.css";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Login = () => {
   return (
-    <div className="login-component">
-      <form action="" className="login-form">
-        <h1>Login</h1>
-        <div className="input-group">
-          <label htmlFor="Email">Email Address</label>
-          <input type="text" />
-          <label htmlFor="Password">Password</label>
-          <input type="text" />
-        </div>
-        <div>
-          <input type="checkbox" name="rememberMe" />
-          <label className="margin-left-xxs" htmlFor="rememberMe">
-            Remember me
-          </label>
-        </div>
-        <a href="">
-          <small>Forgot your password</small>
-        </a>
-        <button className="btn login-btn">Login</button>
-      </form>
-    </div>
+    <>
+      <Navbar />
+      <div className="login-component">
+        <form action="" className="login-form">
+          <h1>Login</h1>
+          <div className="input-group">
+            <label htmlFor="Email">Email Address</label>
+            <input type="text" />
+            <label htmlFor="Password">Password</label>
+            <input type="text" />
+          </div>
+          <div>
+            <input type="checkbox" name="rememberMe" />
+            <label className="margin-left-xxs" htmlFor="rememberMe">
+              Remember me
+            </label>
+          </div>
+          <a href="">
+            <small>Forgot your password</small>
+          </a>
+          <button className="btn login-btn">Login</button>
+        </form>
+      </div>
+    </>
   );
 };
 
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,30 +1,35 @@
+import Navbar from "../../components/Navbar/Navbar";
+
 const Signup = () => {
   return (
-    <div className="login-component">
-      <form action="" className="login-form">
-        <h1>SignUp</h1>
-        <div className="input-group">
-          <label htmlFor="Email">First Name</label>
-          <input type="text" />
-          <label htmlFor="Email">Last Name</label>
-          <input type="text" />
-          <label htmlFor="Email">Email Address</label>
-          <input type="text" />
-          <label htmlFor="Password">Password</label>
-          <input type="text" />
-        </div>
-        <div>
-          <input type="checkbox" name="rememberMe" />
-          <label className="margin-left-xxs" htmlFor="rememberMe">
-            Remember me
-          </label>
-        </div>
-        <a href="">
-          <small>Forgot your password</small>
-        </a>
-        <button className="btn login-btn">SignUp</button>
-      </form>
-    </div>
+    <>
+      <Navbar />
+      <div className="login-component">
+        <form action="" className="login-form">
+          <h1>SignUp</h1>
+          <div className="input-group">
+            <label htmlFor="Email">First Name</label>
+            <input type="text" />
+            <label htmlFor="Email">Last Name</label>
+            <input type="text" />
+            <label htmlFor="Email">Email Address</label>
+            <input type="text" />
+            <label htmlFor="Password">Password</label>
+            <input type="text" />
+          </div>
+          <div>
+            <input type="checkbox" name="rememberMe" />
+            <label className="margin-left-xxs" htmlFor="rememberMe">
+              Remember me
+            </label>
+          </div>
+          <a href="">
+            <small>Forgot your password</small>
+          </a>
+          <button className="btn login-btn">SignUp</button>
+        </form>
+      </div>
+    </>
   );
 };
 
