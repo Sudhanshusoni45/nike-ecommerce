3c10129c3d8efb4991445769a00e6720ef400fc3
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-03-19 18:40:11 +0530

    refactor

---
+++ src/utils/SortData.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+const SortData = ({ products }) => {
+  const [sortedData, setSortedData] = useState("");
+
+  if (e.target.value) {
+  }
+};
+
+export { SortData };
