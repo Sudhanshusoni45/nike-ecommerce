264571be4abdca6d0f851211a81154f84736dc87
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-03-22 18:51:09 +0530

    refactor : added htmlFor to labels & ids to input

--- src/components/FilterSideBar/FilterSideBar.js
+++ src/components/FilterSideBar/FilterSideBar.js
@@ -29,6 +29,7 @@ const FilterSideBar = () => {
               <input
                 type="checkbox"
                 name="category"
+                id="jordans"
                 checked={jordans}
                 onChange={(e) => {
                   e.target.checked
@@ -36,12 +37,14 @@ const FilterSideBar = () => {
                     : filterDispatch({ type: "JORDANS", payload: false });
                 }}
               />
-              <label>Jordan</label>
+
+              <label htmlFor="running">Jordan</label>
             </div>
             <div>
               <input
                 type="checkbox"
                 name="category"
+                id="running"
                 checked={running}
                 onChange={(e) => {
                   e.target.checked
@@ -49,12 +52,13 @@ const FilterSideBar = () => {
                     : filterDispatch({ type: "RUNNING", payload: false });
                 }}
               />
-              <label>Running</label>
+              <label htmlFor="running">Running</label>
             </div>
             <div>
               <input
                 type="checkbox"
                 name="category"
+                id="basketball"
                 checked={basketball}
                 onChange={(e) => {
                   e.target.checked
@@ -62,33 +66,35 @@ const FilterSideBar = () => {
                     : filterDispatch({ type: "BASKETBALL", payload: false });
                 }}
               />
-              <label>Basketball</label>
+              <label htmlFor="basketball">Basketball</label>
             </div>
             <div>
               <input
                 type="checkbox"
                 name="category"
                 checked={football}
+                id="football"
                 onChange={(e) => {
                   e.target.checked
                     ? filterDispatch({ type: "FOOTBALL", payload: true })
                     : filterDispatch({ type: "FOOTBALL", payload: false });
                 }}
               />
-              <label>Football</label>
+              <label htmlFor="football">Football</label>
             </div>
             <div>
               <input
                 type="checkbox"
                 name="category"
                 checked={walking}
+                id="walking"
                 onChange={(e) => {
                   e.target.checked
                     ? filterDispatch({ type: "WALKING", payload: true })
                     : filterDispatch({ type: "WALKING", payload: false });
                 }}
               />
-              <label>Walking</label>
+              <label htmlFor="walking">Walking</label>
             </div>
           </ul>
         </div>
@@ -113,42 +119,46 @@ const FilterSideBar = () => {
             <input
               type="radio"
               name="sortByPrice"
+              id="lowToHigh"
               checked={sortBy === "LOW_TO_HIGH" ? true : false}
               onChange={() => filterDispatch({ type: "LOW_TO_HIGH" })}
             />
-            <label htmlFor="">Price-Low to High</label>
+            <label htmlFor="lowToHigh">Price-Low to High</label>
           </div>
 
           <div>
             <input
+              id="highToLow"
               type="radio"
               name="sortByPrice"
               checked={sortBy === "HIGH_TO_LOW" ? true : false}
               onChange={() => filterDispatch({ type: "HIGH_TO_LOW" })}
             />
-            <label htmlFor="">Price-High to Low</label>
+            <label htmlFor="highToLow">Price-High to Low</label>
           </div>
         </div>
         <h3>Rating</h3>
         <div>
           <input
             type="radio"
+            id="fourStarAbove"
             name="sortByRating"
             checked={rating === "4_STAR_&_ABOVE" ? true : false}
             onChange={() => filterDispatch({ type: "4_STAR_&_ABOVE" })}
           />
-          <label className="margin-left-xxs" htmlFor="">
+          <label className="margin-left-xxs" htmlFor="fourStarAbove">
             4 Stars & above
           </label>
         </div>
         <div>
           <input
+            id="threeStarAbove"
             type="radio"
             name="sortByRating"
             checked={rating === "3_STAR_&_ABOVE" ? true : false}
             onChange={() => filterDispatch({ type: "3_STAR_&_ABOVE" })}
           />
-          <label className="margin-left-xxs" htmlFor="">
+          <label className="margin-left-xxs" htmlFor="threeStarAbove">
             3 Stars & above
           </label>
         </div>
@@ -156,21 +166,23 @@ const FilterSideBar = () => {
           <input
             type="radio"
             name="sortByRating"
+            id="twoStarAbove"
             checked={rating === "2_STAR_&_ABOVE" ? true : false}
             onChange={() => filterDispatch({ type: "2_STAR_&_ABOVE" })}
           />
-          <label className="margin-left-xxs" htmlFor="">
+          <label className="margin-left-xxs" htmlFor="twoStarAbove">
             2 Stars & above
           </label>
         </div>
         <div>
           <input
+            id="oneStarAbove"
             type="radio"
             name="sortByRating"
             checked={rating === "1_STAR_&_ABOVE" ? true : false}
             onChange={() => filterDispatch({ type: "1_STAR_&_ABOVE" })}
           />
-          <label className="margin-left-xxs" htmlFor="">
+          <label className="margin-left-xxs" htmlFor="oneStarAbove">
             1 Stars & above
           </label>
         </div>
