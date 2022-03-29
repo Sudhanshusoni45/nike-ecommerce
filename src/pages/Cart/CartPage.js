6c68148ea324f280d44af1f0159900ee9bb4863a
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-03-29 14:49:49 +0530

    feat: billing functionality

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { CartItem, Navbar } from "../../components";
 import { useCart } from "../../Context/cart-context";
 
 const CartPage = () => {
   const { cartState } = useCart();
+  const [billPrice, setBillPrice] = useState(0);
+
+  const billHandler = () => {
+    let totalPrice = 0;
+    cartState.forEach((element) => {
+      totalPrice = totalPrice + element.qty * element.price;
+    });
+    console.log("totalPrice:", totalPrice);
+    setBillPrice(totalPrice);
+  };
+
+  useEffect(billHandler, [cartState]);
 
   return (
     <>
@@ -29,13 +43,13 @@ const CartPage = () => {
           <h3>Price Details</h3>
           <div className="price-details-grid">
             <span>Price</span>
-            <span>$200</span>
+            <span>{billPrice}</span>
             <span>Discount</span>
             <span>$50</span>
             <span>Delivery Charges</span>
-            <span>0</span>
+            <span>FREE</span>
             <h3>Total Amount</h3>
-            <h3>$150</h3>
+            <h3>{billPrice - 50}</h3>
           </div>
           <p>You will save $50 on this order</p>
           <button className="btn bg-purple">Place Order</button>
