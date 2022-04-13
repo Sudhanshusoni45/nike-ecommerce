451e33f736d8809ee1a8c7e172e0b8ec993254a3
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-04-13 11:20:56 +0530

    feat:add requiresAuth

---
+++ src/components/RequiresAuth/RequiresAuth.js
@@ -0,0 +1,15 @@
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../../Context/auth-context";
+
+const RequiresAuth = ({ children }) => {
+  const { authState } = useAuth();
+  const location = useLocation();
+  const { token } = authState;
+  return token !== null ? (
+    children
+  ) : (
+    <Navigate to="/login" replace state={{ from: location }} />
+  );
+};
+
+export { RequiresAuth };
