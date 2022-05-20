dae6278b9afe1a5adf443e5e67118471944bc4d7
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-05-20 21:23:54 +0530

    fix: fixed import statement

--- src/assests/index.js
+++ src/assests/index.js
@@ -3,6 +3,13 @@ import menCategory from "./menCategory.png";
 import womenCategory from "./womenCategory.png";
 import nikeAirBanner from "./nikeAirBanner.jpg";
 import jordan13Banner from "./jordan13Banner.jpg";
+import shoe from "./shoe.png";
 
-export { heroimage, menCategory, womenCategory, jordan13Banner, nikeAirBanner };
-
+export {
+  heroimage,
+  menCategory,
+  womenCategory,
+  jordan13Banner,
+  nikeAirBanner,
+  shoe,
+};
