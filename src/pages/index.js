1ee6d3619a09ba04caae00a9b8267d930e258223
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-06-12 16:06:43 +0530

    fix:fixed export statements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export { Login } from "./Login/Login.js";
 export { Signup } from "./Signup/Signup.js";
-export { ProductList } from "./productListing/ProductList.js";
-export { Wishlist } from "./wishlist/Wishlist.js";
+export { ProductList } from "./ProductListing/ProductList.js";
+export { Wishlist } from "./Wishlist/Wishlist.js";
 export { CartPage } from "./Cart/CartPage";
 export { Homepage } from "./Homepage/Homepage";
 export { SingleProductPage } from "./SingleProductPage/SingleProductPage";
