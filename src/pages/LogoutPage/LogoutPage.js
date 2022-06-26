413e9b88a0c3445cbce1d991ad2e86af9bddb9a5
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-06-26 12:36:28 +0530

    style:styled logout page

--- src/pages/LogoutPage/LogoutPage.js
+++ src/pages/LogoutPage/LogoutPage.js
@@ -1,9 +1,15 @@
 import { useNavigate } from "react-router-dom";
-import { Navbar } from "../../components";
+import { Footer, Navbar } from "../../components";
 import { useAuth } from "../../context/auth-context";
+import "./logoutPage.css";
 
 const LogoutPage = () => {
-  const { authDispatch } = useAuth();
+  const {
+    authDispatch,
+    authState: {
+      user: { firstName, lastName, email },
+    },
+  } = useAuth();
   const Navigate = useNavigate();
   const logoutHandler = () => {
     authDispatch({ type: "LOGOUT", payload: { user: null, token: null } });
@@ -13,9 +19,29 @@ const LogoutPage = () => {
   return (
     <>
       <Navbar />
-      <button className="btn" onClick={logoutHandler}>
-        Logout
-      </button>
+      <div className="profile-page-container">
+        <div className="profile-container">
+          <div className="profile">
+            <h2 className="profile-page-heading">Profile Details</h2>
+            <div>
+              <div>
+                <h3 className="display-inline-block ">Name : </h3>
+                <span>
+                  {firstName} {lastName}
+                </span>
+              </div>
+              <div>
+                <h3 className="display-inline-block">Email : </h3>
+                <span> {email}</span>
+              </div>
+            </div>
+            <h2 className="profile-page-heading">Account</h2>
+            <button className="btn logout-btn " onClick={() => logoutHandler()}>
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
